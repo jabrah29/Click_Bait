c20755e6ee340d3501337f4ec9bb3e1197001144
jacob <jabrah29@uic.edu>
2018-08-06 17:17:59 -0400

    files removed

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from lib.flask.app import FlaskHandler
 from lib.flask.resources.hello_world import HelloWorld
-import os
 
-config_name = os.getenv('APP_SETTINGS')
 
 flask = FlaskHandler()
 flask.add_resource(HelloWorld,'/')
