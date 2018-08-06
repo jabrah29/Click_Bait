df9223c394efb34f3da6396ef5803d1eda1c4d84
jacob <jabrah29@uic.edu>
2018-08-06 14:50:15 -0400

    patch

--- lib/flask/app.py
+++ lib/flask/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'test'}
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
